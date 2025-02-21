239a4b5b0768b29de59b67403ca07051c46ae1d6
Aleix Díaz <aleix@kumointernet.com>
2025-02-21 18:00:42 +0100

    Initial commit

---
+++ src/main/java/com/aleix/XposeAPI/XposeApiApplication.java
@@ -0,0 +1,13 @@
+package com.aleix.XposeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XposeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XposeApiApplication.class, args);
+	}
+
+}
