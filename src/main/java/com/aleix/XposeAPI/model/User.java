7eb658dadabe8a0addfc0d9b6f6add9c84c68ce8
Aleix Díaz <aleix@kumointernet.com>
2025-03-26 17:28:13 +0100

    fix: cors config and username not required

--- src/main/java/com/aleix/XposeAPI/model/User.java
+++ src/main/java/com/aleix/XposeAPI/model/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(nullable = false)
     private String surname;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
     @Column(nullable = false, unique = true)
